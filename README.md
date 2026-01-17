87f264d5c9bb90a8292894b8ee3d804f72e77576
Samuel Rinnetmäki <samuel@iki.fi>
2026-01-17 23:02:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# International Patient Summary as a digital credential in a digital wallet
+
+Demo: Issue International Patient Summary credential using Procivis One Core.
+
+Based on [Procivis One Core API Reference](https://docs.procivis.ch/reference/core/procivis-one-core-api).
+
+Test a [running instance](https://ips-demo.trustregistry.eu/) with the [Procivis Wallet](https://www.procivis.ch/components/procivis-one-wallet).
---
+++ server.js
@@ -0,0 +1,91 @@
+import  express from "express";
+import { agent } from './agent.js'
+
+const VALIDITY_MS = 365 * 24 * 60 * 60 * 1000 // credential validity time in milliseconds
+
+const config = {
+  "server_port": process.env.IPS_SERVER_PORT || 9627
+}
+
+const app = express();
+app.use(express.json({ limit: '1024mb' }))
+app.use(express.static('public'))
+
+app.post('/issue', async (req, res) => {
+  if (!req.body.content) {
+    res.status(400).json({ error: 'Missing required content' })
+    return false
+  }
+  const content = JSON.parse(req.body.content)
+  const claimValues = {}
+  if (content && content.entry && Array.isArray(content.entry)) {
+    for (const entry of content.entry) {
+      if (entry.resource && entry.resource.resourceType == 'Composition') {
+        for (const section of entry.resource.section) {
+          if (!section.title || !section.text || !section.text.div) {
+            continue
+          }
+          const key = "section" + section.title.replace(/\s+/g, '')
+          claimValues[key] = section.text.div.replace(/<[^>]+>/g, '')
+        }
+      }
+    }
+  }
+  const credentialParams = {
+    credentialSchemaId: agent.schemas.credential.id,
+    issuerDid: agent.dids[0],
+    issuerKey: agent.keys[0],
+    protocol: req.body.protocol || 'OPENID4VCI_FINAL1',
+    claimValues: []
+  }
+  agent.schemas.credential.claims.forEach(claim => {
+    let value
+    if (claim.key == 'fullData') {
+      value = content
+    }
+    else {
+      value = claimValues[claim.key] || ''
+    }
+    if (value) {
+      credentialParams.claimValues.push({
+        claimId: claim.id,
+        value: value,
+        path: `${claim.key}`
+      })
+    }
+  })
+  console.log(JSON.stringify(credentialParams, null, 2))
+  const offer = await agent.issueCredential(credentialParams)
+  if (offer) {
+    res.json(offer)
+    return true
+  }
+  res.status(500).json({ error: 'Failed to create credential offer' })
+})
+
+app.post('/verify', async (req, res) => {
+  const proofParams = {
+    proofSchemaId: agent.schemas.proof.id,
+    verifierDid: agent.dids[0],
+    protocol: req.body.protocol || 'OPENID4VP_FINAL1'
+  }
+  const request = await agent.requestCredential(proofParams)
+  if (request) {
+    res.json(request)
+    return true
+  }
+  res.status(500).json({ error: 'Failed to create credential request' })
+})
+
+app.get('/status/:id', async (req, res) => {
+  const status = await agent.getStatus(req.params.id)
+  if (status) {
+    res.json(status)
+    return true
+  }
+  res.status(500).json({ error: `Failed to check status for ${req.params.id}` })
+})
+
+app.listen(config.server_port, () => {
+  console.log(`Server is running on port ${config.server_port}`)
+})
